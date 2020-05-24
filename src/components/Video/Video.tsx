9e370f0eb01826e0c8d95fd04df3c98b75fbb3ee
MichaelDuo <michaeldong21@gmail.com>
2020-05-24 13:28:00 -0700

    update Video player

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import videojs from 'video.js';
+import videojs, {VideoJsPlayerOptions} from 'video.js';
 import 'video.js/dist/video-js.css';
 
 interface Props {
@@ -7,41 +7,39 @@ interface Props {
 	webm?: string;
 }
 
+const options: VideoJsPlayerOptions = {
+	autoplay: false,
+	controls: true,
+	sources: [],
+};
 function Video(props: Props): JSX.Element {
 	const [videoNode, setVideoNode] = useState<HTMLVideoElement | null>(null);
 	const [player, setPlayer] = useState<videojs.Player | null>(null);
 	useEffect(() => {
 		if (!videoNode) return;
-		const player = videojs(
-			videoNode,
+		options.sources = [
 			{
-				autoplay: true,
-				controls: true,
-				sources: [
-					{
-						src: props.mp4 || '',
-						type: 'video/mp4',
-					},
-				],
+				src: props.mp4 || '',
+				type: 'video/mp4',
 			},
-			function onPlayerReady() {
-				console.log('onPlayerReady');
-				(window as any).player = player;
-			}
-		);
-		setPlayer(player);
-		return;
+		];
+		const p = videojs(videoNode, options, function onPlayerReady() {
+			console.log('onPlayerReady');
+			(window as any).player = p;
+		});
+		setPlayer(p);
+		return (): void => {
+			if (player) player.dispose();
+		};
 	}, [videoNode]);
 
 	return (
-		<div>
-			<div data-vjs-player>
-				<video
-					width="600"
-					ref={(node): void => setVideoNode(node)}
-					className="video-js"
-				></video>
-			</div>
+		<div data-vjs-player>
+			<video
+				width="600"
+				ref={(node): void => setVideoNode(node)}
+				className="video-js"
+			></video>
 		</div>
 	);
 }
