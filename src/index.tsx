19c626cd7dc9f4574420d3fca5466a025a99d51b
Michael Dong <30283874+MichaelDuo@users.noreply.github.com>
2020-05-23 22:03:00 -0700

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Page1 from './pages/page1';
+
+ReactDOM.render(
+	<div>
+		<Page1 />
+	</div>,
+	document.getElementById('app')
+);
---
+++ src/pages/page1.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import './style.scss';
+
+function Test(): JSX.Element {
+	return <div>Test Page test123 123</div>;
+}
+
+export default Test;
